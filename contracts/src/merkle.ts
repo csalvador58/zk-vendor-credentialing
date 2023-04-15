e26ef87985694a54cb6a7f515964d5e9bbf748c4
Chris Salvador <csalvador58@gmail.com>
2023-04-16 01:05:23 +0900

    functional zk verification

--- contracts/src/merkle.ts
+++ contracts/src/merkle.ts
@@ -19,6 +19,7 @@ import {
   MerkleTree,
   MerkleWitness,
   shutdown,
+  CircuitString,
 } from 'snarkyjs';
 
 await isReady;
@@ -27,24 +28,19 @@ const doProofs = true;
 
 class MyMerkleWitness extends MerkleWitness(8) {}
 
-class Account extends CircuitValue {
-  @prop publicKey: PublicKey;
-  @prop points: UInt32;
+class Record extends CircuitValue {
+  @prop status: CircuitString;
 
-  constructor(publicKey: PublicKey, points: UInt32) {
-    super(publicKey, points);
-    this.publicKey = publicKey;
-    this.points = points;
+  constructor(status: CircuitString) {
+    super(status);
+    this.status = status;
   }
 
   hash(): Field {
     return Poseidon.hash(this.toFields());
   }
-
-  addPoints(n: number): Account {
-    return new Account(this.publicKey, this.points.add(n));
-  }
 }
+
 // initiate tree root in order to tell the contract about our off-chain storage
 let initialCommitment: Field = Field(0);
 /*
@@ -65,37 +61,19 @@ class VendorCredential extends SmartContract {
   }
 
   @method
-  verifyCredential(
-    credentialStatus: Field,
-    account: Account,
-    path: MyMerkleWitness
-  ) {
-    // this is our hash! its the hash of the preimage "22", but keep it a secret!
-    let target = Field(
-      '17057234437185175411792943285768571642343179330449434169483610110583519635705'
-    );
+  verifyCredential(credentialData: Record, path: MyMerkleWitness) {
+    // gets the current root of the tree
+    const root = Tree.getRoot();
 
-    // if our credentialStatus preimage hashes to our target, credential is verified!
-    Poseidon.hash([credentialStatus]).assertEquals(target);
+    // calculates the root of the witness
+    let credentialDataHash = credentialData.hash();
+    const calculatedRoot = path.calculateRoot(credentialDataHash);
 
-    // // we fetch the on-chain commitment
-    // let commitment = this.commitment.get();
-    // this.commitment.assertEquals(commitment);
-
-    // // we check that the account is within the committed Merkle Tree
-    // path.calculateRoot(account.hash()).assertEquals(commitment);
-
-    // // we update the account and grant one point!
-    // let newAccount = account.addPoints(1);
-
-    // // we calculate the new Merkle Root, based on the account changes
-    // let newCommitment = path.calculateRoot(newAccount.hash());
-
-    // this.commitment.set(newCommitment);
+    calculatedRoot.assertEquals(root);
   }
 }
 
-type Names = 'Bob' | 'Alice' | 'Charlie' | 'Olivia';
+type VendorData = 'Address' | 'SSN' | 'VC01' | 'VC02';
 
 let Local = Mina.LocalBlockchain({ proofsEnabled: doProofs });
 Mina.setActiveInstance(Local);
@@ -104,34 +82,44 @@ let initialBalance = 10_000_000_000;
 let feePayerKey = Local.testAccounts[0].privateKey;
 let feePayer = Local.testAccounts[0].publicKey;
 
-// the zkapp account
-let zkappKey = PrivateKey.random();
+// the zkapp record
+let zkappKey = PrivateKey.fromBase58(
+  'EKEcnhmtY6ehzn6jwFVXSjcibzPrWjrHSfQVMgYFuDhhdyampDdX'
+);
+console.log('zkappKey:');
+console.log(zkappKey.toBase58());
 let zkappAddress = zkappKey.toPublicKey();
 
+// *****************************************************************
 // this map serves as our off-chain in-memory storage
-let Accounts: Map<string, Account> = new Map<Names, Account>();
+let Records: Map<string, Record> = new Map<VendorData, Record>();
 
-let bob = new Account(Local.testAccounts[0].publicKey, UInt32.from(0));
-let alice = new Account(Local.testAccounts[1].publicKey, UInt32.from(0));
-let charlie = new Account(Local.testAccounts[2].publicKey, UInt32.from(0));
-let olivia = new Account(Local.testAccounts[3].publicKey, UInt32.from(0));
+let address = new Record(CircuitString.fromString('1234 Main St'));
+let ssn = new Record(CircuitString.fromString('999775555'));
+let vc01 = new Record(CircuitString.fromString('MedicalRecord01'));
+let vc02 = new Record(CircuitString.fromString('MedicalRecord02'));
 
-Accounts.set('Bob', bob);
-Accounts.set('Alice', alice);
-Accounts.set('Charlie', charlie);
-Accounts.set('Olivia', olivia);
+Records.set('Address', address);
+Records.set('SSN', ssn);
+Records.set('VC01', vc01);
+Records.set('VC02', vc02);
 
 // we now need "wrap" the Merkle tree around our off-chain storage
 // we initialize a new Merkle Tree with height 8
 const Tree = new MerkleTree(8);
 
-Tree.setLeaf(0n, bob.hash());
-Tree.setLeaf(1n, alice.hash());
-Tree.setLeaf(2n, charlie.hash());
-Tree.setLeaf(3n, olivia.hash());
+Tree.setLeaf(0n, address.hash());
+Tree.setLeaf(1n, ssn.hash());
+Tree.setLeaf(2n, vc01.hash());
+Tree.setLeaf(3n, vc02.hash());
 
-// now that we got our accounts set up, we need the commitment to deploy our contract!
+// Set up set of records, generate a commitment before deploying our contract!
 initialCommitment = Tree.getRoot();
+
+console.log('initialCommitment root:');
+console.log(initialCommitment.toString());
+
+// ***********************************************************
 
 let VendorCredentialZkApp = new VendorCredential(zkappAddress);
 console.log('Deploying VendorCredential..');
@@ -147,46 +135,60 @@ let tx = await Mina.transaction(feePayer, () => {
 });
 await tx.sign([feePayerKey, zkappKey]).send();
 
-// console.log('Initial points: ' + Accounts.get('Bob')?.points);
-
 console.log('Verifier checking a credential..');
-await verifierRequest('Bob', 0n, 21);
+console.log('VendorCredentialZkApp.commitment.get()');
+console.log(VendorCredentialZkApp.commitment.get().toString());
 
-// console.log('Final points: ' + Accounts.get('Bob')?.points);
+let verifyAddress = new Record(CircuitString.fromString('1234 Main St'));
+let verifySSN = new Record(CircuitString.fromString('999775555'));
+let verifyVC01 = new Record(CircuitString.fromString('MedicalRecord01'));
+let verifyVC02 = new Record(CircuitString.fromString('MedicalRecord02'));
+
+// Request to verify Address...
+console.log('Request to verify address');
+await verifierRequest(0n, verifyAddress);
+// Request to verify SSN...
+console.log('Request to verify ssn');
+await verifierRequest(1n, verifySSN);
+// Request to verify VC01...
+console.log('Request to verify vc01');
+await verifierRequest(2n, verifyVC01);
+// Request to verify VC02...
+console.log('Request to verify vc02');
+await verifierRequest(3n, verifyVC02);
+
+// Request to verify Address (with incorrect data)...
+console.log(
+  'Request to verify address with incorrect data. Credential verification should fail.'
+);
+let verifyAddressFailTest = new Record(
+  CircuitString.fromString('5678 Main St')
+);
+await verifierRequest(0n, verifyAddressFailTest);
 
 async function verifierRequest(
-  name: Names,
+  //   key: VendorData,
   index: bigint,
-  credentialStatus: number
+  credentialData: Record
 ) {
-  let account = Accounts.get(name)!;
+  //   let record = Records.get(key)!;
   let w = Tree.getWitness(index);
   let witness = new MyMerkleWitness(w);
 
   console.log('Verifier request in process...');
   try {
     let tx = await Mina.transaction(feePayer, () => {
-      VendorCredentialZkApp.verifyCredential(
-        Field(credentialStatus),
-        account,
-        witness
-      );
+      VendorCredentialZkApp.verifyCredential(credentialData, witness);
     });
     await tx.prove();
     await tx.sign([feePayerKey, zkappKey]).send();
 
     console.log('Credentials verified!');
   } catch (ex: any) {
-    console.log('Vendor credential not verified: ');
-    console.log('*********************PRIVATE MESSAGE*********************');
-    console.log(ex.message);
-    console.log('*********************************************************');
+    console.log(
+      'Vendor credential does not match. Credentials are not verified'
+    );
   }
-
-  //   // if the transaction was successful, we can update our off-chain storage as well
-  //   account.points = account.points.add(1);
-  //   Tree.setLeaf(index, account.hash());
-  //   VendorCredentialZkApp.commitment.get().assertEquals(Tree.getRoot());
 }
 
 console.log('Shutting down');
